d08ede1efa23a340a5fd1d17a766b3166618034d
Melih <mlhtncc@gmail.com>
2018-07-31 11:04:08 +0300

    Changed data path. Now, it is system-independent

--- src/datamining/DataMining.java
+++ src/datamining/DataMining.java
@@ -1,27 +1,32 @@
 package datamining;
 
+import java.io.File;
+
 /**
  *
  * @author tnc
  */
 public class DataMining
 {
+    static String dataPath = System.getProperty("user.home") + File.separator +
+            "Desktop" + File.separator + "data" + File.separator + "E0.csv";
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args)
     {
-//        League league = CSV_Reader.read("/home/tnc/Desktop/data_mining/E0.csv");
-//
-//        Match m = league.getCurrMatch();
-//        m.print();
-//        league.playCurrMatch();
-//        league.printTable();
-//
-//        m = league.getCurrMatch();
-//        m.print();
-//        league.playCurrMatch();
-//        league.printTable();
+        League league = CSV_Reader.read(dataPath);
+
+        Match m = league.getCurrMatch();
+        m.print();
+        league.playCurrMatch();
+        league.printTable();
+
+        m = league.getCurrMatch();
+        m.print();
+        league.playCurrMatch();
+        league.printTable();
     }
     
 }

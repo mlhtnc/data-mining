72e09ac637e688790b4c92ad19f4301100ba4e1b
Melih <mlhtncc@gmail.com>
2018-08-14 00:25:24 +0300

    More data

--- src/datamining/DataMining.java
+++ src/datamining/DataMining.java
@@ -15,14 +15,17 @@ public class DataMining
     static final String DATA_PATH = System.getProperty("user.home") + File.separator +
             "Desktop" + File.separator + "data" + File.separator;
     
-    static final String E0 = DATA_PATH + "E0.csv";
-    static final String T1 = DATA_PATH + "T1.csv";
+    static final String _17_18 = DATA_PATH + "17-18" + File.separator + "E0.csv";
+    static final String _16_17 = DATA_PATH + "16-17" + File.separator + "E0.csv";
+    static final String _15_16 = DATA_PATH + "15-16" + File.separator + "E0.csv";
+
     
     public static void main(String[] args)
     {
         ArrayList<League> leagues = new ArrayList<>();        
-        leagues.add(CSV_Reader.read(E0));
-        leagues.add(CSV_Reader.read(T1));
+        leagues.add(CSV_Reader.read(_17_18));
+        leagues.add(CSV_Reader.read(_16_17));
+        leagues.add(CSV_Reader.read(_15_16));
         
         NeuralNetwork nn = new NeuralNetwork(
             new int[]{6, 40, 3},
@@ -36,7 +39,7 @@ public class DataMining
         
         League[] leagueArr = leagues.toArray(new League[leagues.size()]);
         Trainer trainer = new Trainer(nn, leagueArr, 0.1f);
-        trainer.train(10000);      
+        trainer.train(5000);      
         trainer.test();       
     }
 }
